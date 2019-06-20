b73a7d15da98226cb55a7786037488ba8d9f48f9
Nikita Esipov <38347951+dasaderto@users.noreply.github.com>
2019-06-21 02:39:07 +0300

    Front Header

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Header from "../../modules/Header";
+
+import "./Home.scss";
+
+
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default Home;
